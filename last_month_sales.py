1af441a65208fee367d9c1c153d3675b6f4935a9
Joby Ingram-Dodd <joby@protonmail.com>
2021-02-03 17:33:42 +0000

    x

--- last_month_sales.py
+++ last_month_sales.py
@@ -3,26 +3,16 @@ import numpy as np
 
 df = pd.read_csv('./competitive-data-science-predict-future-sales/sales_train.csv')
 df = df.fillna(0)
-#print(df.info())
 df_oct = df[df.date_block_num == 33]
-#print(df_oct.info())
-shop_items = df_oct.groupby(by=["item_id","shop_id"])
-
-month = pd.DataFrame({'shop_id':[],'item_id':[],'item_cnt_month':[]})
-for s, i in shop_items:
-    sh = s[1]
-    it = s[0]
-    sa = i.item_cnt_day.sum()
-    month = month.append({'shop_id':sh,'item_id':it,'item_cnt_month':sa}, ignore_index=True)
-
-month.item_cnt_month = month.item_cnt_month.clip(lower=0, upper=20)
-
+oct15 = df_oct.groupby(by=["item_id","shop_id"])['item_cnt_day'].agg(['sum']).reset_index()
 te_df = pd.read_csv('./competitive-data-science-predict-future-sales/test.csv')
-
-pred_df = pd.merge(te_df, month, how='left')
-print("test shape ", te_df.shape )
-print("pred shape ", pred_df.columns)
-sub = pred_df[['ID','item_cnt_month']]
+pred_df = pd.merge(te_df, oct15, how='left', on=["shop_id", "item_id"])
+sub = pred_df[['ID','sum']]
 sub = sub.fillna(0)
-sub.to_csv('./competitive-data-science-predict-future-sales/last_month_submission.csv', index=False)
+
+sub = sub['sum'].clip(lower=0, upper=20)
+subs = pd.read_csv('./competitive-data-science-predict-future-sales/sample_submission.csv')
+subs.item_cnt_month = sub.sum
+subs.to_csv('./competitive-data-science-predict-future-sales/last_month_submission.csv', index=False)
 print(sub.shape)
+print(subs.shape)
